aec20a2d4cfe57706e45980c940fa9e2adf810ac
Ghamri-Aya <159073278+Ghamri-Aya@users.noreply.github.com>
2024-03-02 13:27:58 +0100

    Update Exo2CorrectionTest.java

--- src/test/java/Exo2CorrectionTest.java
+++ src/test/java/Exo2CorrectionTest.java
@@ -1,3 +1,4 @@
+import org.example.Exo2;
 import org.example.Exo2Correction;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,25 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Exo2CorrectionTest {
 
     @Test
+    public void LengthOfTheTwoStringAreInequal() {
+        assertFalse(Exo2Correction.isAnagram("azerty", "az"));
+    }
+
+    @Test
+    public void SameLetterqAndDifferentFrequency() {
+        assertFalse(Exo2Correction.isAnagram("aabbcc", "aabbc"));
+    }
+
+    @Test
     public void IsAnagramReturnsTrue() {
-        //assertTrue(Exo2.isAnagram("", ""));
+        assertTrue(Exo2Correction.isAnagram("", ""));
         assertTrue(Exo2Correction.isAnagram("listen", "silent"));
         assertTrue(Exo2Correction.isAnagram("LISTEN", "silent"));
         assertTrue(Exo2Correction.isAnagram("LISTEN", "SILENT"));
-        assertTrue(Exo2Correction.isAnagram("School master", "The classroom"));
+        assertTrue(Exo2Correction.isAnagram("LIS TEN", "S ILE NT"));
     }
 
     @Test
     public void IsAnagramReturnsFalse() {
         assertFalse(Exo2Correction.isAnagram("azerty", "querty"));
-        assertFalse(Exo2Correction.isAnagram("azerty", "az"));
-        assertFalse(Exo2Correction.isAnagram("aabbcc", "aabbc"));
+    }
+
+
+    @Test
+    public void ThrowExceptionIfOneOfTheStringsIsNull() {
+        assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram(null, ""));
+        assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram("", null));
+        assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram(null, null));
     }
 
     @Test
-    public void ThrowExceptionIfOneOfTheStringsIsNullOrBoth() {
-        assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram(null, ""));
-        assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram("", null));
+    public void ThrowExceptionIfBothAreNull() {
         assertThrows(NullPointerException.class, () -> Exo2Correction.isAnagram(null, null));
     }
 }
